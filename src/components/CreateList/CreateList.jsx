20ac7a5557c6cc949ae716ef314b564d73fa01e4
DozortsevaMarie <dozortsevamasha@gmail.com>
2021-01-28 14:46:07 +0300

    changed favicon and logo

--- src/components/CreateList/CreateList.jsx
+++ src/components/CreateList/CreateList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Empty } from "antd";
 import styles from "./CreateList.module.css";
 import {
@@ -15,28 +15,11 @@ import SuccessAlert from "../SuccessAlert/SuccessAlert";
 import AddItem from "../AddItem/AddItem";
 
 const CreateList = (props) => {
-  // const { changeProductValue } = props;
-  // const [state, setActive] = useState(false);
-  // const [value, setValue] = useState();
   const [alertVisible, setAlertVisible] = useState(false);
-  // const [inputValue, setInputValue] = useState();
 
   const handleAlertClose = () => {
     setAlertVisible(false);
   };
-
-  /*useEffect(() => {
-    changeProductValue(value);
-  }, [value, changeProductValue]);*/
-
-  // useEffect(() => setInputValue(props.inputElement), [props.inputElement]);
-
- /* const addProductToAList = () => {
-    if (value !== null && value.length > 0) {
-      props.addProduct(value);
-      setValue(null);
-    }
-  };*/
 
   return (
     <div>
@@ -44,41 +27,14 @@ const CreateList = (props) => {
       <div className={styles.alert}>
         {alertVisible && <SuccessAlert handleAlertClose={handleAlertClose} />}
       </div>
-      {/*<Form
-        className={classNames(styles.input, {
-          [styles.active]: state,
-        })}
-        onSubmit={addProductToAList}
-      >
-        <Input
-          size={"middle"}
-          placeholder={"Введите наименование товара"}
-          onChange={(e) => setValue(e.target.value)}
-          value={inputValue}
-          autoFocus={true}
-          onPressEnter={addProductToAList}
-        />
-        <Button type="primary" onClick={addProductToAList}>
-          Добавить
-        </Button>
-      </Form>
-      <Button
-        onClick={() => setActive(true)}
-        className={classNames(styles.addBtn, {
-          [styles.hidden]: state,
-        })}
-      >
-       +<span className={styles.addText}>Добавить товар</span>
-      </Button>*/}
       <AddItem changeProductValue={props.changeProductValue}
                addProduct={props.addProduct}
                inputElement={props.inputElement}
       />
       <div className={styles.emptyListImage}>
-        {props.shoppingList.length === 0 && (
-          <Empty description={"Список пуст"} />
-        )}
-        {props.shoppingList.length !== 0 && (
+        { props.shoppingList.length === 0 && (
+          <Empty description={"Список пуст"} />) }
+        { props.shoppingList.length !== 0 && (
           <div>
             <TableComponent
               shoppingList={props.shoppingList}
@@ -91,8 +47,7 @@ const CreateList = (props) => {
               setAlertVisible={setAlertVisible}
               isAuth={props.isAuth}
             />
-          </div>
-        )}
+          </div>) }
       </div>
     </div>
   );
